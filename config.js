23365186478430f783cd0376f53105dba5adcf1e
heliang <heliang@duyao001.com>
2018-04-02 10:59:07 +0800

    添加代码

--- config.js
+++ config.js
@@ -6,7 +6,8 @@
 var config={
     mongo:{
         //数据库链接地址
-        url : 'mongodb://localhost:27017/myBlog',
+        url : 'mongodb://1cf07db61bc24:53dfa19ee30a4@192.168.1.90:30419/d6898f0b17d54',
+        // url : 'mongodb://localhost:27017/myBlog',
         //分页
         pageSize :7
     }
