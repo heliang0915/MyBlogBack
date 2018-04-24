7e20ae18c89aea317f693e52088a4915490a4eaf
heliang <heliang@duyao001.com>
2018-04-24 15:29:27 +0800

    调整成内存分页

--- db/modelManager.js
+++ db/modelManager.js
@@ -1,8 +1,6 @@
-
 let  baseDao = require('./base');
 let util=require('util');
 let modules=["menuModel","blogModel","channelModel","commentModel","rightModel","roleModel","userModel","zanModel"];
-// var modelManager={};
 modules.forEach(function (item,index){
     let fnPrefix= item.slice(0,item.indexOf('Model'));
     let fn=`function ${fnPrefix}Manager(){
@@ -10,30 +8,7 @@ modules.forEach(function (item,index){
         };
         util.inherits(${fnPrefix}Manager,baseDao); 
         exports.${fnPrefix}Manager = ${fnPrefix}Manager;
-        // modelManager['${fnPrefix}Manager']=${fnPrefix}Manager;
-        // console.log(modelManager);
         `
-
     eval(fn);
-
-    // if(index==modules.length-1){
-
-        // console.log("代码执行了..."+JSON.stringify(fnMap));
-
-         // console.log(Object.keys(modelManager));
-    // }
 });
-    // console.log(fnMap['commentManager']);
-    // console.log(fnMap['commentManager']);
-
-    // let {commentManager}=modelManager;
-    // console.log(new commentManager());
-// console.log(modelManager);
-// exports=modelManager;
-
-
-
-// function menuManager(){
-//     baseDao.call(this,"menuModel")
-// }
 
